b0b9aeaaf46ef294a2389f81583a524c9a543d6c
siwuxie <cwl990110@163.com>
2024-10-29 00:05:37 +0800

    refactor(main): 重构主函数并添加配置解析功能
    - 新增 parse_config 函数用于解析命令行参数
    - 修改 main 函数以使用新的配置解析逻辑
    - 优化代码结构，提高可读性和可维护性

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,22 @@ use std::{env, fmt, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
-    let file_content = fs::read_to_string(&args[1]).expect("fs read error");
+    let (query,file_path) = parse_config(&args);
+    let file_content =fs::read_to_string(file_path).expect("fs read error");
     println!("{file_content}")
 }
 
-// run : cargo run src/file.txt
+fn parse_config(configs: &Vec<String> ) ->(&str,&str){
+    let query = &configs[1];
+    let filename = &configs[2];
+    println!("Searching for {} in {}", query, filename);
+    return (query,filename)
+}
+
+// run : cargo run query src/file.txt
+
+/*
+2024-10-29 00:04:53进度：
+看到了：https://course.rs/basic-practice/refactoring.html中的
+修改后，类似 String::new 的调用，我们可以通过 Config::new 来创建一个实例，看起来代码是不是更有那味儿了 ：）
+*/
